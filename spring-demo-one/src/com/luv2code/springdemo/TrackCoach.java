fbbbbdd3052f63c3d19101e9b3b626a6cb6070ec
yousulin <yousulin@unixecure.com.tw>
2022-10-10 11:08:49 +0800

    initial commit

---
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+	
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {}
+	
+	// define a constructor for dependency injection
+	public TrackCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just Do It: " + fortuneService.getFortune();
+	}
+
+}
